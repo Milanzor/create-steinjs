22e15a25c34c27dc2e6f4b35bacfc9467807027d
Milan <milanvanas@gmail.com>
2018-02-10 23:11:19 +0100

    First setup, seems to be working

---
+++ bin/create.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+const path = require('path');
+const {spawnSync} = require('child_process');
+
+const rimraf = require('rimraf');
+
+
+// Root directory of the project
+const rootPath = path.resolve(__dirname, '..', '..', 'stein-app');
+
+const boilerplateRepoUrl = 'https://github.com/Milanzor/stein-boilerplate.git';
+
+console.log('Fetching boilerplate from git');
+// Use git to get our boilerplate
+spawnSync('git', ['clone', boilerplateRepoUrl, rootPath]);
+
+console.log('Removing project files from boilerplate');
+
+// Remove files we dont want
+rimraf.sync(path.join(rootPath, '.git'));
+rimraf.sync(path.join(rootPath, 'README.md'));
+
+
+// Installs package deps
+console.log('Installing dependencies, this can take a bit');
+spawnSync('yarn', [], {cwd: rootPath});
+
+// Initialize a new git repo
+console.log('Initializing git repo');
+spawnSync('git', ['init'], {cwd: rootPath});
+
+// All done!
+console.log('All done, have fun!');
